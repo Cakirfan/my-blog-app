9379b0688e9a26a845b5265794f495da1059e158
irfan <cakirfan.001@gmail.com>
2023-10-18 18:39:12 +0200

    RankData added

---
+++ src/helpers/data.js
@@ -0,0 +1,32 @@
+export const RankData = [
+  {
+    rankId: 1,
+    fullname: "Ricardo Flanagan",
+    job: "Digital Artist",
+    image: "images/creator-1.webp",
+  },
+  {
+    rankId: 2,
+    fullname: "Cristina Shaw",
+    job: "Photoshop Expert",
+    image: "images/creator-2.jpeg",
+  },
+  {
+    rankId: 3,
+    fullname: "Clara Jones",
+    job: "Backend Developer",
+    image: "images/creator-3.jpeg",
+  },
+  {
+    rankId: 4,
+    fullname: "Mark Davis",
+    job: "Illustrator Designer",
+    image: "images/creator-4.jpg",
+  },
+  {
+    rankId: 5,
+    fullname: "Shara Anderson",
+    job: "Food Vlogegrs",
+    image: "images/creator-5.webp",
+  },
+];
