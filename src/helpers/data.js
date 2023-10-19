3ac284ee98f4eb59d00430821e7ea7cc4c6772f6
irfan <cakirfan.001@gmail.com>
2023-10-19 21:49:32 +0200

    sidebar data added

--- src/helpers/data.js
+++ src/helpers/data.js
@@ -1,3 +1,7 @@
+import { FaHome, FaUserCircle, FaSignOutAlt } from "react-icons/fa";
+import { BiSolidHelpCircle, BiSolidMessageAltDetail } from "react-icons/bi";
+import { RiLockPasswordFill, RiSettings3Fill } from "react-icons/ri";
+
 export const RankData = [
   {
     rankId: 1,
@@ -28,5 +32,52 @@ export const RankData = [
     fullname: "Shara Anderson",
     job: "Food Vlogegrs",
     image: "images/creator-5.webp",
+  },
+];
+
+
+//? Sidebar Data
+export const data = [
+  {
+    id: 1,
+    href: "/",
+    name: "home",
+    icon: <FaHome className="icon" />,
+  },
+  {
+    id: 2,
+    href: "help",
+    name: "help",
+    icon: <BiSolidHelpCircle className="icon" />,
+  },
+  {
+    id: 3,
+    href: "messages",
+    name: "messages",
+    icon: <BiSolidMessageAltDetail className="icon" />,
+  },
+  {
+    id: 4,
+    href: "password",
+    name: "password",
+    icon: <RiLockPasswordFill className="icon" />,
+  },
+  {
+    id: 5,
+    href: "profile",
+    name: "profile",
+    icon: <FaUserCircle className="icon" />,
+  },
+  {
+    id: 6,
+    href: "settings",
+    name: "settings",
+    icon: <RiSettings3Fill className="icon" />,
+  },
+  {
+    id: 7,
+    href: "signOut",
+    name: "sign out",
+    icon: <FaSignOutAlt className="icon" />,
   },
 ];
