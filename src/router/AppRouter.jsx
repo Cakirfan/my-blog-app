0c7007a707f5f7b872c1f1c270c221243de5676b
irfan <cakirfan.001@gmail.com>
2023-10-19 22:24:26 +0200

    Home Items added

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,19 +1,41 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "../App.css";
 import Home from "../pages/home/Home";
 import Login from "../pages/login/Login";
 import Register from "../pages/register/Register";
 import Navbar from "../components/navbar/Navbar";
+import Messages from "../components/home/Messages";
+import Password from "../components/home/Password";
+import Profile from "../components/home/Profile";
+import Settings from "../components/home/Settings";
+import SignOut from "../components/home/SignOut";
+
+import Sidebar from "../components/sidebar/Sidebar";
+import Blog from "../components/home/Blog";
 
 const AppRouter = () => {
+  
+  // const [currentUser, setCurrentUser] = useState(false);
+
   return (
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <div className="container">
+        <Sidebar />
+        <main className="main">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="blog" element={<Blog />} />
+            <Route path="messages" element={<Messages />} />
+            <Route path="password" element={<Password />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="signOut" element={<SignOut />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </main>
+      </div>
     </BrowserRouter>
   );
 };
